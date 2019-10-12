206af6e3e697c6bea68322bea8e9828da0917a79
tahreen zeba <tahreenzeba1@gmail.com>
2019-10-12 15:35:42 -0400

    Updates

---
+++ precache-manifest.24f9689a4ffa1e4b7193c090dfad5842.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e0c8d416c7066d1adf6d1d029c4148",
+    "url": "/clicky-game-react/index.html"
+  },
+  {
+    "revision": "00c4dafa6769fc2316d2",
+    "url": "/clicky-game-react/static/css/main.f8f645af.chunk.css"
+  },
+  {
+    "revision": "d84dfe67aa6f4c0cf283",
+    "url": "/clicky-game-react/static/js/2.46ec92cf.chunk.js"
+  },
+  {
+    "revision": "00c4dafa6769fc2316d2",
+    "url": "/clicky-game-react/static/js/main.11b7df5e.chunk.js"
+  },
+  {
+    "revision": "2d33cb7c862f2dda2842",
+    "url": "/clicky-game-react/static/js/runtime-main.f5bb3f7a.js"
+  }
+]);
