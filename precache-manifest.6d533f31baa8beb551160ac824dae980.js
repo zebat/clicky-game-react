b2e3bf70233e186b3126b53de4a1b583554887c2
Zeba <50170893+zeba-tahreen@users.noreply.github.com>
2022-08-27 18:00:47 -0400

    Updates

---
+++ precache-manifest.6d533f31baa8beb551160ac824dae980.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2efd0104f4d4d1cf316",
+    "url": "/clicky-game-react/static/css/main.66136524.chunk.css"
+  },
+  {
+    "revision": "c2efd0104f4d4d1cf316",
+    "url": "/clicky-game-react/static/js/main.c2efd010.chunk.js"
+  },
+  {
+    "revision": "46b07cda37bc7abf97e6",
+    "url": "/clicky-game-react/static/js/1.46b07cda.chunk.js"
+  },
+  {
+    "revision": "1ffa1f4187ecf54ec1d4",
+    "url": "/clicky-game-react/static/js/runtime~main.1ffa1f41.js"
+  },
+  {
+    "revision": "3ade652dbb5b840d211eb01dc35bce3f",
+    "url": "/clicky-game-react/index.html"
+  }
+];
